5c69c833111500add8fe757b25192dfc97a09999
OGSegu <seguisreal@gmail.com>
2020-09-01 02:37:11 +0300

    Added promocode system

--- src/main/java/Main/DigiParse.java
+++ src/main/java/Main/DigiParse.java
@@ -1,2 +1,52 @@
-package Main;public class DigiParse {
+package Main;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class DigiParse {
+    private static final String ID_SELLER = "409342";
+    private static final String PASSWORD = "7018022";
+
+    public static Object[] checkCode(String code) {
+        Object[] result = new Object[3];
+        String md5 = DigestUtils.md5Hex(String.format("%s:%s:%s", ID_SELLER, code, PASSWORD));
+        String requestString = String.format("{" +
+                "\"id_seller\":\"%s\"," +
+                "\"unique_code\":\"%s\"," +
+                "\"sign\":\"%s\"" +
+                "}", ID_SELLER, code, md5);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(requestString))
+                .uri(URI.create("https://www.oplata.info/xml/check_unique_code.asp?"))
+                .setHeader("Content-Type", "\"text/json\" | \"text/xml\" ")
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (response == null) return result;
+        JSONObject jsonObject = new JSONObject(response.body());
+        System.out.println(jsonObject.toString());
+        try {
+            result[2] = jsonObject.getString("amount");
+            result[1] = jsonObject.getInt("cnt_goods");
+            JSONArray array = jsonObject.getJSONArray("options");
+            result[0] = array.getJSONObject(0).getString("user_data");
+        } catch (JSONException e) {
+            return result;
+        }
+        return result;
+    }
 }
